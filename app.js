4736c68426827641de2496eb4d757e4f6935545f
Animesh-weavers <animesh.dey@weavers-web.com>
2022-04-26 13:50:58 +0530

    import and export files

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports={
+    date:29,
+    year:2002,
+    month:'Jan',
+    name:function(n){
+        return (`Name:${n}\nDate:${this.date}\nYear:${this.year}\nMonth:${this.month}`);
+    }
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const app=require('./app');
+console.log(app);
+console.log(app.name('Animesh Dey'));
