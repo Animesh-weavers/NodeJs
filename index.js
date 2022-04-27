dd27ef0bd0322f32bb1f83563e63d05a56d52a09
Animesh-weavers <animesh.dey@weavers-web.com>
2022-04-27 17:47:07 +0530

    render html and json

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
+const { getMaxListeners } = require("process");
 const app = express();
 app.get("", (req, res) => {
   console.log(req.query);
-  res.send([
-    {
-      name: req.query.name,
-      age: req.query.age,
-    },
-  ]);
+  res.send("<h1>Welcome</h1>");
 });
 app.get("/about", (req, res) => {
   res.send("Hello, this is About Page");
 });
 app.get("/help", (req, res) => {
-  res.send("Hello, this is Help Page");
+  res.send([
+    {
+      name: "A",
+      email: "a@gmail.com",
+    },
+    {
+      name: "B",
+      email: "b@gmail.com",
+    },
+  ]);
 });
 
 app.listen(5000);
